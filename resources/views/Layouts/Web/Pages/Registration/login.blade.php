21850ab2399a45baf932593e915e4f82bcbbcc76
thienan <annguyen090198@gmail.com>
2020-12-24 20:25:14 +0700

    test

--- resources/views/Layouts/Web/Pages/Registration/login.blade.php
+++ resources/views/Layouts/Web/Pages/Registration/login.blade.php
@@ -1,5 +1,10 @@
 @extends('Layouts.Web.Pages.app')
 @section('title','Đăng nhập')
+@section('css')
+<style>
+    
+</style>
+@stop
 @section('content')
     @component('Layouts.Web.Components.banner')
         @slot('pageName','Đăng nhập')
